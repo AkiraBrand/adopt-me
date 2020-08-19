c20cd67b48ed60243d3a4c0bf712804c49265515
Ruth Brand <ruthbrandsoprano@gmail.com>
2020-08-18 19:23:16 -0700

    Adds props and destructures them

--- src/app.js
+++ src/app.js
@@ -1,18 +1,29 @@
-const Pet = () => {
+const Pet = ({ name, animal, breed}) => {
   return React.createElement("div", {}, [ //here comes the children
-    React.createElement("h1", {}, "Sparkle Goth"),
-    React.createElement("h2", {}, "Cat"),
-    React.createElement("h2", {}, "Domestic Shorthair")
+    React.createElement("h1", {}, name),
+    React.createElement("h2", {}, animal),
+    React.createElement("h2", {}, breed)
   ]);
 };
 
 const App = () => {
-  return React.createElement("div", { id: "Something Important" }, [
-      React.createElement("h1", {}, "Adopt Me!"),
-      React.createElement(Pet),
-      React.createElement(Pet),
-      React.createElement(Pet)
-    ]);
+  return React.createElement("div", {}, [
+    React.createElement("h1", {}, "Adopt Me!"),
+    React.createElement(Pet, {
+      name: "Junior",
+      animal: "Dog",
+      breed: "Dauschund"
+    }),
+    React.createElement(Pet, {
+      name: "Sparkle Goth",
+      animal: "Cat",
+      breed: "Domestic Shorthair"
+    }),
+    React.createElement(Pet, {
+      name: "Birbie Girl",
+      animal: "Bird",
+      breed: "Cockatiel" })
+  ]);
 };
 
 ReactDOM.render(React.createElement(App), document.getElementById("root"));
